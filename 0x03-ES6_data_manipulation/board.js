d1285d11675dbf9e9a75df0445ceeabf4c8b9be2
Babuhatarii <ochegoharyon@gmail.com>
2024-06-04 14:08:05 +0300

    function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/board.js
@@ -0,0 +1,15 @@
+//create array
+
+let cars = ["BMW", "BENZ", "FORD"];
+//push() adds an element at the end of an array
+//cars.push("LIMO");
+//console.log (cars);
+//pop()removes the last element of the array
+//unshift()adds an element at the beginning of the array
+//shift()removes the first element of the array
+//concat(): Combines two or more arrays.
+
+let spirits = ["KEG", "JD","JW"];
+
+let anasa = cars.concat(spirits);
+console.log(anasa);
